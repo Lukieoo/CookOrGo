bef6138b951589adf7dceab47bdcfd662e0f50f9
lukieoo <pawkrzysciak@gmail.com>
2023-10-22 21:19:20 +0200

    reset configuration

--- backend/cookorgo/src/test/java/com/anioncode/cookorgo/CookOrGoApplicationTests.java
+++ backend/cookorgo/src/test/java/com/anioncode/cookorgo/CookOrGoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cookorgo.cookorgo;
+package com.anioncode.cookorgo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CookorgoApplicationTests {
+class CookOrGoApplicationTests {
 
 	@Test
 	void contextLoads() {
